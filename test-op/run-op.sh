65cdbc794f6539b7e8417e5c2118edd459b82906
nservant <nicolas.servant@curie.fr>
2017-12-07 14:17:02 +0100

    [DEV] Add test-op folder

---
+++ test-op/run-op.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Test dataset - polyA RNA
+../bin/RNApip_cluster -i SAMPLE_PLAN_B249 -o /data/tmp/RNAPIP_OP_B249/ -c CONFIG_Human -n testop_B249
+
+## Test dataset - total RNA
+../bin/RNApip_cluster -i SAMPLE_PLAN_L81 -o /data/tmp/RNAPIP_OP_L81/ -c CONFIG_Human -n testop_L81
+
+## Test dataset - Mouse samples
+../bin/RNApip_cluster -i SAMPLE_Gendrel -o /data/tmp/RNAPIP_Gendrel -c CONFIG_Mouse -n testop_Gendrel
+
