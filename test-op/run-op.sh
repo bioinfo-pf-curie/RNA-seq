820c04fa0d90325406a4d35d8165d99e5958b6b9
nservant <nicolas.servant@curie.fr>
2018-07-12 09:59:24 +0200

    add config files

--- test-op/run-op.sh
+++ test-op/run-op.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ## Test dataset - polyA RNA
-../bin/RNApip_cluster -i SAMPLE_PLAN_B249 -o /data/tmp/RNAPIP_OP_B249/ -c CONFIG_Human.hg19 -n testop_B249
+../bin/RNApip_cluster -i SAMPLE_PLAN_B249 -o /data/tmp/RNAPIP_OP_B249/ -c config-files/CONFIG_Human.hg19 -n testop_B249
 
 ## Test dataset - total RNA
-../bin/RNApip_cluster -i SAMPLE_PLAN_L81 -o /data/tmp/RNAPIP_OP_L81/ -c CONFIG_Human.hg38 -n testop_L81
+../bin/RNApip_cluster -i SAMPLE_PLAN_L81 -o /data/tmp/RNAPIP_OP_L81/ -c config-files/CONFIG_Human.hg38 -n testop_L81
 
 ## Test dataset - Mouse samples
-../bin/RNApip_cluster -i SAMPLE_PLAN_Gendrel -o /data/tmp/RNAPIP_Gendrel/ -c CONFIG_Mouse.mm10 -n testop_Gendrel
+../bin/RNApip_cluster -i SAMPLE_PLAN_Gendrel -o /data/tmp/RNAPIP_Gendrel/ -c config-files/CONFIG_Mouse.mm10 -n testop_Gendrel
 
